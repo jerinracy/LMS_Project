394cb3156228cdffa977cfaeaec9d7bbad58ca36
Jarin SIfat <jerinsifat01@gmail.com>
2026-04-11 02:51:46 +0600

    Fixing Db Config in Settings.py

--- Backend/CourseManagementSystem/settings.py
+++ Backend/CourseManagementSystem/settings.py
@@ -107,8 +107,8 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
