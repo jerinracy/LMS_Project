8dfdd9d4ebfa2d9c543934062c83bc8784a22f44
Jarin SIfat <jerinsifat01@gmail.com>
2026-04-17 04:19:39 +0600

    Fixing Media full URL issue

--- Backend/courses/serializers.py
+++ Backend/courses/serializers.py
@@ -23,20 +23,34 @@ class CourseListSerializer(serializers.ModelSerializer):
     instructor = UserSerializer(read_only=True)
     lesson_count = serializers.IntegerField(source='lessons.count', read_only=True)
     is_enrolled = serializers.BooleanField(read_only=True)
+    thumbnail = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = ('id', 'title', 'description', 'thumbnail', 'instructor', 'created_at', 'lesson_count', 'is_enrolled')
+
+    def get_thumbnail(self, obj):
+        request = self.context.get('request')
+        if obj.thumbnail:
+            return request.build_absolute_uri(obj.thumbnail.url)
+        return None
 
 
 class CourseDetailSerializer(serializers.ModelSerializer):
     instructor = UserSerializer(read_only=True)
     lessons = LessonSerializer(many=True, read_only=True)
     is_enrolled = serializers.BooleanField(read_only=True)
+    thumbnail = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = ('id', 'title', 'description', 'thumbnail', 'instructor', 'created_at', 'lessons', 'is_enrolled')
+
+    def get_thumbnail(self, obj):
+        request = self.context.get('request')
+        if obj.thumbnail:
+            return request.build_absolute_uri(obj.thumbnail.url)
+        return None
 
 
 class CourseWriteSerializer(serializers.ModelSerializer):
